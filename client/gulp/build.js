c8e75a7422b84885331f007b1664a2297a652186
Luiz Amorim <luiz.simples@gmail.com>
2015-10-19 14:01:28 -0200

    Add sbAdmin2 A Bootstrap 3 admin theme featuring powerful jQuery plugins to extend the functionality of the Bootstrap framework.

---
+++ client/gulp/build.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var gulp           = require('gulp');
+var gutil          = require('gulp-util');
+var envify         = require('envify');
+var buffer         = require('vinyl-buffer');
+var assign         = require('lodash.assign');
+var source         = require('vinyl-source-stream');
+var watchify       = require('watchify');
+var reactify       = require('reactify');
+var sourcemaps     = require('gulp-sourcemaps');
+var browserify     = require('browserify');
+var browserSync    = require('browser-sync');
+var browserSyncSpa = require('browser-sync-spa');
+
+var browser    = 'default';
+var pathApp    = __dirname.concat('/../src/app.js');
+var pathBundle = __dirname.concat('/../assets/js');
+var customOpts = {
+  entries: [pathApp],
+  debug: true
+};
+
+var opts = assign({}, watchify.args, customOpts);
+var b    = watchify(browserify(opts));
+
+b.transform(reactify);
+b.transform(envify);
+
+function bundle() {
+  return b.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(pathBundle));
+}
+
+browserSync.instance = false;
+
+b.on('update', bundle);
+b.on('log', gutil.log);
+b.on('bundle', function(e) {
+  e.on('end', function() {
+    gutil.log(gutil.colors.green('Arquivo js/bundle.js compilado com sucesso!'));
+
+    if (browserSync.instance) {
+      browserSync.reload();
+      return;
+    }
+
+    var server = {
+      baseDir: __dirname.concat('/..')
+    };
+
+    browserSync.instance = browserSync.init({
+      startPath: '/',
+      server: server,
+      browser: browser
+    });
+  });
+});
+
+browserSync.use(browserSyncSpa({
+  selector: '[react-app]'
+}));
+
+gulp.task('build', bundle);
--- client/js/app.js
+++
@@ -1,7 +0,0 @@
-/*globals document:false */
-'use strict';
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-var KaurisClient = require('./components/KaurisClient.react');
-ReactDOM.render(<KaurisClient />, document.getElementById('KaurisClient'));
